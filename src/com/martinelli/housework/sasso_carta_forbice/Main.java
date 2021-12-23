0ab104ed4604c4d053d4d428a6ed01e08d8700ba
alessandro <martinellialessandro04@gmail.com>
2021-12-23 16:40:16 +0100

    Fatti i primi 2 es di compito

---
+++ src/com/martinelli/housework/sasso_carta_forbice/Main.java
@@ -0,0 +1,45 @@
+package com.martinelli.housework.sasso_carta_forbice;
+
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        while (true) {
+            System.out.println("Inserisci scelta o * per uscire:");
+            String choice = scanner.nextLine();
+
+            if (choice.equals("*")) {
+                return;
+            }
+
+            SassoCartaForbice u1, u2;
+
+            try {
+                u1 = SassoCartaForbice.valueOf(choice.toUpperCase());
+            } catch (Exception e) {
+                System.out.println("Scelta non valida");
+                continue;
+            }
+
+            System.out.println("Inserisci seconda scelta:");
+            String choice2 = scanner.nextLine();
+
+            try {
+                u2 = SassoCartaForbice.valueOf(choice2.toUpperCase());
+            } catch (Exception e) {
+                System.out.println("Scelta non valida");
+                continue;
+            }
+
+            if (u1.vinceContro(u2)) {
+                System.out.println("Vince " + u1.getNome());
+            } else if (u1.perdeContro(u2)) {
+                System.out.println("Vince " + u2.getNome());
+            } else {
+                System.out.println("Pareggio!");
+            }
+        }
+    }
+}
